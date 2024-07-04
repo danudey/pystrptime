0c932f179a9a524b24305b0e420348110541b7de
Daniel Fox <dan.fox@tigera.io>
2024-07-04 11:06:15 -0700

    Add more test workflow stuff

--- check_release_notes.py
+++ check_release_notes.py
@@ -3,13 +3,18 @@
 import os
 import re
 import sys
+import json
 
 import github
+
+GITHUB_EVENT_PATH = os.getenv("GITHUB_EVENT_PATH")
+
+github_data = json.load(open(GITHUB_EVENT_PATH))
 
 gh = github.Github()
 
 repo_name = os.getenv("GH_REPO")
-pr_number = int(os.getenv("NUMBER"))
+pr_number = github_data['number']
 
 repo = gh.get_repo(repo_name)
 pr = repo.get_pull(pr_number)
