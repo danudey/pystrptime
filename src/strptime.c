4ca8ec25da0766b15e085139bb8f8cc29159b9d6
Daniel Fox <dan.fox@tigera.io>
2026-03-23 11:17:59 -0700

    Reformat with clang-format

--- src/strptime.c
+++ src/strptime.c
@@ -4,59 +4,45 @@
 #include <pyerrors.h>
 #include <time.h>
 
-static PyObject *
-my_strptime(PyObject *self, PyObject *args)
-{
-    const char *datetime;
-    const char *formatstr;
-    struct tm tm = {0};
-    const char *extraneous_chars;
-    PyObject *py_datetime = NULL;
+static PyObject *my_strptime(PyObject *self, PyObject *args) {
+  const char *datetime;
+  const char *formatstr;
+  struct tm tm = {0};
+  const char *extraneous_chars;
+  PyObject *py_datetime = NULL;
 
-    if (!PyArg_ParseTuple(args, "ss", &datetime, &formatstr))
-        return NULL;
+  if (!PyArg_ParseTuple(args, "ss", &datetime, &formatstr))
+    return NULL;
 
-    extraneous_chars = strptime(datetime, formatstr, &tm);
+  extraneous_chars = strptime(datetime, formatstr, &tm);
 
-    if (extraneous_chars == NULL) {
-        PyErr_SetString(PyExc_ValueError, "Date/time format did not match format string");
-        return (PyObject *) NULL;
-    }
+  if (extraneous_chars == NULL) {
+    PyErr_SetString(PyExc_ValueError,
+                    "Date/time format did not match format string");
+    return (PyObject *)NULL;
+  }
 
-    py_datetime = PyDateTime_FromDateAndTime(
-            tm.tm_year + 1900,
-            tm.tm_mon + 1,
-            tm.tm_mday,
-            tm.tm_hour,
-            tm.tm_min,
-            tm.tm_sec,
-            0);
+  py_datetime =
+      PyDateTime_FromDateAndTime(tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday,
+                                 tm.tm_hour, tm.tm_min, tm.tm_sec, 0);
 
-    if (py_datetime == NULL) {
-        return NULL;
-    }
+  if (py_datetime == NULL) {
+    return NULL;
+  }
 
-    return py_datetime;
+  return py_datetime;
 }
 
 static PyMethodDef StrpMethods[] = {
-    {"strptime",  my_strptime, METH_VARARGS,
-     "Parse a time string."},
-    {NULL, NULL, 0, NULL}        /* Sentinel */
+    {"strptime", my_strptime, METH_VARARGS, "Parse a time string."},
+    {NULL, NULL, 0, NULL} /* Sentinel */
 };
 
 static struct PyModuleDef strptimemodule = {
-    PyModuleDef_HEAD_INIT,
-    "strptime",
-    "A module for parsing date/time values much faster.",
-    -1,
-    StrpMethods
-};
+    PyModuleDef_HEAD_INIT, "strptime",
+    "A module for parsing date/time values much faster.", -1, StrpMethods};
 
-PyMODINIT_FUNC
-PyInit_strptime(void)
-{
-    PyDateTime_IMPORT;
-    return PyModule_Create(&strptimemodule);
+PyMODINIT_FUNC PyInit_strptime(void) {
+  PyDateTime_IMPORT;
+  return PyModule_Create(&strptimemodule);
 }
-
