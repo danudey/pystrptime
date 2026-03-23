3329525b95bc1d72769ec24a4b7b27c3ea00c3c4
Daniel Fox <dan.fox@tigera.io>
2026-03-23 10:59:11 -0700

    Ruff format

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from distutils.core import setup, Extension
 
-strptime = Extension('strptime',
-                     sources=['src/strptime.c'])
+strptime = Extension("strptime", sources=["src/strptime.c"])
 
-setup(name='strptime',
-      version='1.0',
-      description='Parse a string in C',
-      ext_modules=[strptime])
+setup(
+    name="strptime",
+    version="1.0",
+    description="Parse a string in C",
+    ext_modules=[strptime],
+)
